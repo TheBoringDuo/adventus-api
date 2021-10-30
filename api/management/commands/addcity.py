f2baf205fb6ff9b4851ad04964c8d45af6e384c0
Nicola Nicolov <nicola-smart@hotmail.com>
2021-10-31 00:13:36 +0300

    Add destID scraper in addcity

--- api/management/commands/addcity.py
+++ api/management/commands/addcity.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 from django.core.management.base import BaseCommand, CommandError
 from api.models import Hotel, Country, City
 
@@ -15,7 +16,9 @@ class Command(BaseCommand):
         city = options['city']
         destID = options['destID']
 
-        if country == "" or city == "" or destID == "":
+        # make destID optional but still keep the option to
+        # set it manually
+        if country == "" or city == "": # or destID == "":
             self.stdout.write(self.style.ERROR("Not enough parameters passed"))
             return
         else:    
@@ -25,6 +28,24 @@ class Command(BaseCommand):
                 except:
                     self.stdout.write(self.style.ERROR("There is no record of such country"))
                     return
+                if destID == "":
+                    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'}
+
+                    s = requests.Session()
+                    response = s.get("https://www.booking.com/", headers=headers)
+                    soup=BeautifulSoup(response.content,'lxml').prettify()
+                    csrfToken = str(soup).split("'X-Booking-CSRF': '", 1)[1].split("'",1)[0]
+
+                    headers["X-Booking-Csrf"] = csrfToken
+                    headers["X-Booking-Client-Info"] = "wqhje213213jhk123hRandomshit"
+
+                    url = "https://www.booking.com/autocomplete_csrf?v=1&lang=en-us&aid=eedwqe&pid=ku123dd213&term=" + city + ",%20" + country
+                    response2 = s.get(url, headers=headers)
+
+                    response_json = json.loads(response2.content)
+                    print(response_json['city'][0]['city_name'])
+                    destID = response_json['city'][0]['dest_id']
+
                 City.objects.create(name=city, country=countryObj, destID=destID)
                 self.stdout.write(self.style.SUCCESS("Added a record of a city {} in {} with Destionation ID {}".format(city, country, destID)))
             except Exception as e:
