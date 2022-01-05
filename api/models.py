e8a920985d2bb97336d7fa9f6998bd7e36982235
Yordan <anatomicys@gmail.com>
2022-01-05 17:00:39 +0200

    Extended the hotels model, added a restaurants model #4

--- api/models.py
+++ api/models.py
@@ -14,14 +14,6 @@ class City(models.Model):
     class Meta:
         unique_together = ('name', 'country')
 
-class Hotel(models.Model):
-    name = models.CharField(max_length=50)
-    link = models.TextField()
-    added_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
-    city = models.ForeignKey(City, on_delete=models.PROTECT)
-
-
 class User(AbstractUser):
 	email = models.EmailField(unique=True)
 	isBusinessClient = models.BooleanField(default=False)
@@ -30,3 +22,32 @@ class User(AbstractUser):
 	REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
 	
+class Hotel(models.Model):
+    name = models.CharField(max_length=50)
+    bookingLink = models.TextField(default="")
+    added_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    city = models.ForeignKey(City, on_delete=models.PROTECT)
+    isFetchedFromBooking = models.BooleanField(default=True)
+    description = models.TextField(default="")
+    available = models.BooleanField(default=True) # a field that the business clients can edit - if false, it will be listed at the bottom
+    #but won't be used by adventus for suggestions and planning because it means the hotel isn't available at the moment
+    active = models.BooleanField(default=True) # a field that the admins can edit - if false, it won't be listed anywhere on the platform
+    listed = models.BooleanField(default=True) # a field that the business clients can edit - if false, it won't be listed anywhere on the platform
+    ownedBy = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
+    locLong = models.DecimalField(max_digits=9, decimal_places=6, null=True)
+    locLat = models.DecimalField(max_digits=9, decimal_places=6, null=True)
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=50)
+    added_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    city = models.ForeignKey(City, on_delete=models.PROTECT)
+    description = models.TextField()
+    available = models.BooleanField(default=True) # a field that the business clients can edit - if false, it will be listed at the bottom
+    #but won't be used by adventus for suggestions and planning because it means the hotel isn't available at the moment
+    active = models.BooleanField(default=True) # a field that the admins can edit - if false, it won't be listed anywhere on the platform
+    listed = models.BooleanField(default=True) # a field that the business clients can edit - if false, it won't be listed anywhere on the platform
+    ownedBy = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
+    locLong = models.DecimalField(max_digits=9, decimal_places=6, null=True)
+    locLat = models.DecimalField(max_digits=9, decimal_places=6, null=True)
--- api/serializers.py
+++ api/serializers.py
@@ -17,34 +17,34 @@ class CitySerializer(serializers.ModelSerializer):
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-	email = serializers.EmailField(required=True,validators=[UniqueValidator(queryset=User.objects.all())])
-	password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    email = serializers.EmailField(required=True,validators=[UniqueValidator(queryset=User.objects.all())])
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
 
-	class Meta:
-		model = User
-		fields = ['email', 'password', 'first_name', 'last_name']
-		extra_kwags = {
-			'first_name': {'required': True},
-			'last_name': {'required': True}
-		}
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'first_name', 'last_name']
+        extra_kwags = {
+            'first_name': {'required': True},
+            'last_name': {'required': True}
+        }
 
-	def create(self, validated_data, user=None):
-		if user is None:
-			username = str(uuid.uuid4()) # we do not need the username field, but django.contrib.auth requires it to exist and be unique so we are generating a random uuid for each user to handle that 
-			user = User.objects.create(username=username, email=validated_data["email"],
-			first_name=validated_data["first_name"], last_name=validated_data["last_name"])
-		user.set_password(validated_data["password"])
-		user.save()
-		return user
+    def create(self, validated_data, user=None):
+        if user is None:
+            username = str(uuid.uuid4()) # we do not need the username field, but django.contrib.auth requires it to exist and be unique so we are generating a random uuid for each user to handle that 
+            user = User.objects.create(username=username, email=validated_data["email"],
+            first_name=validated_data["first_name"], last_name=validated_data["last_name"])
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
 
 
 class BusinessRegisterSerializer(RegisterSerializer):
-	def create(self, validated_data):
-		username = str(uuid.uuid4())
-		user = User.objects.create(username=username, email=validated_data["email"],
-		first_name=validated_data["first_name"], last_name=validated_data["last_name"], isBusinessClient=True)
-		user = super().create(validated_data, user)
-		return user
+    def create(self, validated_data):
+        username = str(uuid.uuid4())
+        user = User.objects.create(username=username, email=validated_data["email"],
+        first_name=validated_data["first_name"], last_name=validated_data["last_name"], isBusinessClient=True)
+        user = super().create(validated_data, user)
+        return user
 
 
-	
+    
