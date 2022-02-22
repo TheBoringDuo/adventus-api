a901076e1e05c9e1d8e9dbaf005bf1f03ffaf5ea
Yordan <anatomicys@gmail.com>
2022-02-22 16:23:09 +0200

    changes to admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,15 @@
+import imp
 from django.contrib import admin
 from .models import User
 from django.contrib.auth.admin import UserAdmin
-
+from models import Country, City, Restaurant, Hotel
 
 # Register your models here.
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Country)
+admin.site.register(Restaurant)
+admin.site.register(City)
+admin.site.register(Hotel)
+
 
