6ce49e05942ee48ed31e2abd0facbb6101d0b1c4
Yordan <anatomicys@gmail.com>
2022-02-23 10:36:48 +0200

    destID not unique anymore

--- api/models.py
+++ api/models.py
@@ -19,7 +19,7 @@ class Country(models.Model):
 class City(models.Model):
     name = models.CharField(max_length=30)
     country = models.ForeignKey(Country, on_delete=models.RESTRICT)
-    destID = models.CharField(max_length=12, unique=True, null=False)
+    destID = models.CharField(max_length=12, null=False)
     
     def __str__(self):
         return "{}, {}".format(self.name, self.country.name)
